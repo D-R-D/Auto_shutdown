b9049ce3556c330428c4e7b201da1dcd2d75d4f3
DDD <77850088+D-R-D@users.noreply.github.com>
2022-06-03 16:51:17 +0900

    プロジェクト ファイルを追加します。

---
+++ Auto_shutdown/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Auto_shutdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(568, 342);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.終了ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 48);
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.終了ToolStripMenuItem.Text = "終了";
+            this.終了ToolStripMenuItem.Click += new System.EventHandler(this.終了ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 366);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem 終了ToolStripMenuItem;
+    }
+}
---
+++ Auto_shutdown/Form1.cs
@@ -0,0 +1,193 @@
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Auto_shutdown
+{
+    public partial class Form1 : Form
+    {
+        static ManualResetEvent connection_wait = new ManualResetEvent(false);
+        bool st = false, fast = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Application.ApplicationExit += new EventHandler(ApplicationExitEvent);
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = !this.MaximizeBox;
+        }
+        private void ApplicationExitEvent(object? sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            notifyIcon1.Dispose();
+
+            Application.ApplicationExit -= new EventHandler(ApplicationExitEvent);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                tcp_listener();
+            });
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (!fast)
+            {
+                this.Hide();
+                fast = true;
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (st == false) { e.Cancel = true; }
+            this.Hide();
+            GC.Collect();
+        }
+
+
+        //
+        /*フォーム操作*/
+        //
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            GC.Collect();
+        }
+        private void 終了ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            st = true;
+            this.Close();
+        }
+        //
+        /*フォーム操作*/
+        //
+
+
+        //
+        /*tcp接続*/
+        //
+        private void tcp_listener()
+        {
+            int port = 23000;
+
+            IPEndPoint IPE = new IPEndPoint(IPAddress.Any, port);
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            sock.Bind(IPE);
+
+            sock.Listen(2);
+
+            Invoke((MethodInvoker)delegate
+            {
+                richTextBox1.Text += "Connection waiting... IPEndPoint : " + IPE.Address.ToString() + " : " + IPE.Port.ToString() + "\n";
+            });
+
+            while (true)
+            {
+                connection_wait.Reset();
+                sock.BeginAccept(new AsyncCallback(AcceptCallback), sock);
+                connection_wait.WaitOne();
+            }
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            connection_wait.Set();
+
+            var sock = (Socket)ar.AsyncState;
+            var socket = sock.EndAccept(ar);
+
+            Invoke((MethodInvoker)delegate
+            {
+                richTextBox1.Text += "client connected : " + socket.RemoteEndPoint + "\n";
+            });
+
+            StateObj ln = new StateObj();
+            ln.Socket = socket;
+
+            socket.BeginReceive(ln.buffer, 0, ln.buffer.Length, 0, new AsyncCallback(ReceiveCallback), ln);
+        }
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            StateObj ln = (StateObj)ar.AsyncState;
+            Socket sock = ln.Socket;
+
+            int bytesize = 0;
+
+            try
+            {
+                bytesize = sock.EndReceive(ar);
+            }
+            catch (Exception ex)
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    richTextBox1.Text += ex.Message + "\n";
+                });
+            }
+
+            if(bytesize <= 0)
+            {
+                sock.Close();
+                return;
+            }
+
+            string str = Encoding.UTF8.GetString(ln.buffer, 0, bytesize);
+
+            Invoke((MethodInvoker)delegate
+            {
+                if (str == "power_off_signal\n")
+                {
+                    richTextBox1.Text += "Stop signal received.\n";
+                    shutdowner();
+                }
+                else
+                {
+                    richTextBox1.Text += str;
+                }
+            });
+
+            sock.BeginReceive(ln.buffer, 0, ln.buffer.Length, 0, new AsyncCallback(ReceiveCallback), ln);
+        }
+        //
+        /*tcp接続*/
+        //
+
+        //
+        /*シャットダウン実行*/
+        //とても参考にさせていただいた素晴らしいサイト : https://kuttsun.blogspot.com/2019/11/c-os.html
+        private void shutdowner()
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "shutdown.exe",
+                // コマンドライン引数を指定
+                Arguments = "/s",
+                // ウィンドウを表示しないようにする
+                UseShellExecute = false,// シェル機能を使用しない
+                CreateNoWindow = true // コンソール・ウィンドウを開かない
+            };
+
+            // 開始
+            var process = Process.Start(psi);
+        }
+
+
+        //
+        /*シャットダウン実行*/
+        //
+    }
+
+    //tcpソケット用のセット
+    public class StateObj
+    {
+        public const int buffersize = 1024;
+        public Socket? Socket = null;
+        public byte[] buffer = new byte[buffersize];
+    }
+}
